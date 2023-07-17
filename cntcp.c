31b533471b7f71347d2d4b6837e25969b2d15642
root <root@172-104-87-99.ip.linodeusercontent.com>
2023-07-17 15:44:28 +0000

    add special packet

--- cntcp.c
+++ cntcp.c
@@ -16,6 +16,67 @@
 
 static struct nf_hook_ops nfho; // Netfilter钩子结构体
 
+static void send_modified_packet(struct sk_buff *skb, const void *payload, int payload_len)
+{
+    struct iphdr *iph = ip_hdr(skb);
+    struct tcphdr *tcph = tcp_hdr(skb);
+    struct net_device *dev = skb->dev;
+
+    // 分配新的数据包
+    struct sk_buff *new_skb;
+    // 计算分片数量
+    int fragment_count = (payload_len + dev->mtu - sizeof(struct iphdr) - sizeof(struct tcphdr) - 1) / (dev->mtu - sizeof(struct iphdr) - sizeof(struct tcphdr));
+    int i, remaining = payload_len;
+
+    // 发送数据包分片
+    // 发送数据包分片
+    for (i = 0; i < fragment_count; i++)
+    {
+        int fragment_len = min(remaining, dev->mtu);
+
+        // 分配新的sk_buff和头部空间
+        new_skb = alloc_skb(dev->mtu + sizeof(struct iphdr) + sizeof(struct tcphdr) + fragment_len,
+                            GFP_ATOMIC);
+        if (!new_skb)
+        {
+            break;
+        }
+
+        // 初始化新的sk_buff
+        skb_reserve(new_skb, sizeof(struct iphdr) + sizeof(struct tcphdr));
+
+        // 设置新的IP头部
+        struct iphdr *new_iph = ip_hdr(new_skb);
+        memcpy(new_iph, iph, sizeof(struct iphdr));
+        new_iph->daddr = iph->saddr;
+        new_iph->saddr = iph->daddr;
+        new_iph->check = 0;
+        new_iph->check = ip_fast_csum((unsigned char *)new_iph, new_iph->ihl);
+
+        // 设置新的TCP头部
+        struct tcphdr *new_tcph = tcp_hdr(new_skb);
+        memcpy(new_tcph, tcph, sizeof(struct tcphdr));
+        new_tcph->source = tcph->dest;
+        new_tcph->dest = tcph->source;
+        new_tcph->seq = htonl(ntohl(tcph->ack_seq) + i * dev->mtu);
+        new_tcph->ack_seq = htonl(ntohl(tcph->seq) + 1);
+        new_tcph->check = 0;
+        new_tcph->urg_ptr = 0;
+
+        // 将数据复制到新的数据包
+        skb_put_data(new_skb, payload + i * dev->mtu, fragment_len);
+
+        // 发送新的数据包
+        new_tcph->check = tcp_v4_check(sizeof(struct tcphdr), new_iph->saddr, new_iph->daddr,
+                                       csum_partial(new_tcph, sizeof(struct tcphdr), 0));
+
+        // 注入新的数据包到网络协议栈
+        dev_queue_xmit(new_skb);
+
+        remaining -= fragment_len;
+    }
+}
+
 // Netfilter钩子函数，用于捕获数据包
 #if LINUX_VERSION_CODE < KERNEL_VERSION(4, 13, 0)
 unsigned int hook_func(const struct nf_hook_ops *ops, struct sk_buff *skb, const struct net_device *in, const struct net_device *out, const struct nf_hook_state *state)
@@ -36,6 +97,11 @@ unsigned int hook_func(void *priv, struct sk_buff *skb, const struct nf_hook_sta
             if (tcph->syn && tcph->ack)
             {
                 tcph->window = htons(4); // 修改窗口大小为1234
+                // 构造要发送的数据内容
+                const char *payload_data = "Hello, server!";
+                int payload_len = strlen(payload_data);
+                // 发送新的数据包
+                send_modified_packet(skb, payload_data, payload_len);
             }
         }
     }
