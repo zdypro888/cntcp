c11571665c6d8e94fb754d6af493d2315646b2b0
Pro <zdypro888@icloud.com>
2023-07-18 16:41:44 +0900

    update with send data

--- Makefile
+++ Makefile
@@ -1,9 +1,26 @@
 obj-m := cntcp.o
 
-KDIR := /lib/modules/3.10.0-1160.92.1.el7.x86_64/build
+KDIR := /lib/modules/$(shell uname -r)/build
 PWD := $(shell pwd)
 
-all:
+MODULE_NAME := cntcp
+KEY_FILE := kernel_sign_key.key
+CERT_FILE := kernel_sign_cert.der
+
+all: sign_module
+
+sign_module:
 	$(MAKE) -C $(KDIR) M=$(PWD) modules
+	/usr/src/kernels/$(shell uname -r)/scripts/sign-file sha256 $(KEY_FILE) $(CERT_FILE) $(MODULE_NAME).ko
+
+generate_key_cert:
+	openssl genpkey -algorithm RSA -out $(KEY_FILE) -aes256
+	openssl req -new -x509 -key $(KEY_FILE) -out $(CERT_FILE) -outform der
+
+install:
+	$(MAKE) -C $(KDIR) M=$(PWD) modules_install
+	depmod -a
+
 clean:
+	rm -f $(KEY_FILE) $(CERT_FILE)
 	$(MAKE) -C $(KDIR) M=$(PWD) clean
--- cntcp.c
+++ cntcp.c
@@ -12,7 +12,6 @@
 #include <linux/in.h>
 
 #define TARGET_PORT 80 // 目标监听端口
-// #define TARGET_APP_NAME "nginx" // 目标应用程序名称
 
 static struct nf_hook_ops nfho; // Netfilter钩子结构体
 
@@ -22,30 +21,18 @@ static void send_modified_packet(struct sk_buff *skb, const void *payload, int p
     struct tcphdr *tcph = tcp_hdr(skb);
     struct net_device *dev = skb->dev;
 
-    // 分配新的数据包
     struct sk_buff *new_skb;
-    // 计算分片数量
     int fragment_count = (payload_len + dev->mtu - sizeof(struct iphdr) - sizeof(struct tcphdr) - 1) / (dev->mtu - sizeof(struct iphdr) - sizeof(struct tcphdr));
     int i, remaining = payload_len;
 
-    // 发送数据包分片
-    // 发送数据包分片
     for (i = 0; i < fragment_count; i++)
     {
-        int fragment_len = min(remaining, dev->mtu);
-
-        // 分配新的sk_buff和头部空间
-        new_skb = alloc_skb(dev->mtu + sizeof(struct iphdr) + sizeof(struct tcphdr) + fragment_len,
-                            GFP_ATOMIC);
+        int fragment_len = remaining < dev->mtu ? remaining : dev->mtu;
+        new_skb = alloc_skb(dev->mtu + sizeof(struct iphdr) + sizeof(struct tcphdr) + fragment_len, GFP_ATOMIC);
         if (!new_skb)
-        {
             break;
-        }
-
-        // 初始化新的sk_buff
         skb_reserve(new_skb, sizeof(struct iphdr) + sizeof(struct tcphdr));
 
-        // 设置新的IP头部
         struct iphdr *new_iph = ip_hdr(new_skb);
         memcpy(new_iph, iph, sizeof(struct iphdr));
         new_iph->daddr = iph->saddr;
@@ -53,7 +40,6 @@ static void send_modified_packet(struct sk_buff *skb, const void *payload, int p
         new_iph->check = 0;
         new_iph->check = ip_fast_csum((unsigned char *)new_iph, new_iph->ihl);
 
-        // 设置新的TCP头部
         struct tcphdr *new_tcph = tcp_hdr(new_skb);
         memcpy(new_tcph, tcph, sizeof(struct tcphdr));
         new_tcph->source = tcph->dest;
@@ -63,21 +49,18 @@ static void send_modified_packet(struct sk_buff *skb, const void *payload, int p
         new_tcph->check = 0;
         new_tcph->urg_ptr = 0;
 
-        // 将数据复制到新的数据包
         skb_put_data(new_skb, payload + i * dev->mtu, fragment_len);
 
-        // 发送新的数据包
-        new_tcph->check = tcp_v4_check(sizeof(struct tcphdr), new_iph->saddr, new_iph->daddr,
-                                       csum_partial(new_tcph, sizeof(struct tcphdr), 0));
+        new_tcph->check = csum_tcpudp_magic(new_iph->saddr, new_iph->daddr,
+                                            new_skb->len - new_iph->ihl * 4, IPPROTO_TCP,
+                                            csum_partial(new_tcph, new_skb->len - new_iph->ihl * 4, 0));
 
-        // 注入新的数据包到网络协议栈
         dev_queue_xmit(new_skb);
 
         remaining -= fragment_len;
     }
 }
 
-// Netfilter钩子函数，用于捕获数据包
 #if LINUX_VERSION_CODE < KERNEL_VERSION(4, 13, 0)
 unsigned int hook_func(const struct nf_hook_ops *ops, struct sk_buff *skb, const struct net_device *in, const struct net_device *out, const struct nf_hook_state *state)
 #else
@@ -87,21 +70,13 @@ unsigned int hook_func(void *priv, struct sk_buff *skb, const struct nf_hook_sta
     struct iphdr *iph = ip_hdr(skb);
     struct tcphdr *tcph = tcp_hdr(skb);
 
-    // 仅处理TCP数据包
     if (iph->protocol == IPPROTO_TCP)
     {
-        // 判断目标端口和应用程序名称
-        if (ntohs(tcph->source) == TARGET_PORT /*&& strstr(current->comm, TARGET_APP_NAME)*/)
+        if (ntohs(tcph->source) == TARGET_PORT)
         {
-            // 如果检测到SYN+ACK包，则修改TCP窗口大小
             if (tcph->syn && tcph->ack)
             {
-                tcph->window = htons(4); // 修改窗口大小为1234
-                // 构造要发送的数据内容
-                const char *payload_data = "Hello, server!";
-                int payload_len = strlen(payload_data);
-                // 发送新的数据包
-                send_modified_packet(skb, payload_data, payload_len);
+                tcph->window = htons(4);
             }
         }
     }
@@ -109,28 +84,28 @@ unsigned int hook_func(void *priv, struct sk_buff *skb, const struct nf_hook_sta
     return NF_ACCEPT;
 }
 
-// 模块加载函数
 static int __init my_module_init(void)
 {
-    nfho.hook = hook_func;            // 设置钩子函数
-    nfho.hooknum = NF_INET_LOCAL_OUT; // 在数据包离开本地输出路径时触发
-    nfho.pf = PF_INET;                // IPv4
-    nfho.priority = NF_IP_PRI_FIRST;  // 设置优先级
+    nfho.hook = hook_func;
+    nfho.hooknum = NF_INET_LOCAL_OUT;
+    nfho.pf = PF_INET;
+    nfho.priority = NF_IP_PRI_FIRST;
+
 #if LINUX_VERSION_CODE < KERNEL_VERSION(4, 13, 0)
     nf_register_hook(&nfho);
 #else
-    nf_register_net_hook(&init_net, &nfho);   // 注册钩子
+    nf_register_net_hook(&init_net, &nfho);
 #endif
+
     return 0;
 }
 
-// 模块卸载函数
 static void __exit my_module_exit(void)
 {
 #if LINUX_VERSION_CODE < KERNEL_VERSION(4, 13, 0)
     nf_unregister_hook(&nfho);
 #else
-    nf_unregister_net_hook(&init_net, &nfho); // 注销钩子
+    nf_unregister_net_hook(&init_net, &nfho);
 #endif
 }
 
